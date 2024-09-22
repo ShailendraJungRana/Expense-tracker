3d8a3c996170122a8aaf06dae871913649fd93e5
Arun jR <cannon.arun1234@gmail.com>
2024-09-22 23:23:15 +0545

    Error

--- src/components/Transaction.jsx
+++ src/components/Transaction.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import { GlobalContext } from '../context/GlobalState';
 const Transaction = ({transaction}) => {
+    const {deleteTransaction} = React.useContext(GlobalContext);
     const sign = transaction.amount < 0 ? 'minus' : 'plus';
   return (
     <li className={transaction.amount < 0 ? "minus" : "plus"}>
             {transaction.text} <span>{sign}${Math.abs(transaction.amount)}</span>
-          <button className='delete-btn'>
+          <button onClick={deleteTransaction} className='delete-btn'>
           X
           </button>
           </li>
--- src/context/GlobalState.jsx
+++ src/context/GlobalState.jsx
@@ -16,9 +16,18 @@ import AppReducer from './AppReducer'
     //provider component
     export const GlobalProvider = ({children}) => {
         const [state, dispatch] = useReducer(AppReducer, initialState);
+
+        // Action
+        function deleteTransaction(id){
+            dispatch({
+                type: 'DELETE_TRANSACTION',
+                payload: id
+            })
+        }
         return(
             <GlobalContext.Provider value={{ 
-                transactions: state.transactions }}>
+                transactions: state.transactions, deleteTransaction
+                 }}>
             {children}
           </GlobalContext.Provider>
         );
